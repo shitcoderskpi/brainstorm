dbefd421edc6a430b47a22c72618e2fef644224d
LonerXW <koval.oleksandr@lll.kpi.ua>
2025-03-24 18:00:05 +0200

    AuthReadyWithTests
    PLS dont break
    authorization is ready, registration and authorization tests are ready, basic visual added

--- Brainstorm.Web/Program.cs
+++ Brainstorm.Web/Program.cs
@@ -1,35 +1,55 @@
-using Brainstorm.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite; 
+using Brainstorm.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<BrainstormDbContext>(options =>
+builder.Services.AddDbContext<BrainstormDbContext>(o =>
 {
-    options.UseSqlite("Data Source=brainstorm.db");
+    o.UseSqlite("Data Source=brainstorm.db;Cache=Shared;Mode=ReadWriteCreate");
 });
 
 builder.Services.AddScoped<UserRepository>();
-
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(o =>
+    {
+        o.LoginPath = "/Auth/Login";
+        o.AccessDeniedPath = "/Auth/AccessDenied";
+    });
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BrainstormDbContext>();
+    db.Database.EnsureCreated();
+    db.Database.ExecuteSqlRaw("PRAGMA journal_mode=WAL;");
+}
 
 app.Run();
+
+
+public partial class Program
+{
+}
