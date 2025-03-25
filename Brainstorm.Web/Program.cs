ca7a5256080593b6514017c3af765abfc057358b
inquaterni <m.matzkevich@gmail.com>
2025-03-25 14:54:02 +0200

    Fixed merge conflict issues

--- Brainstorm.Web/Program.cs
+++ Brainstorm.Web/Program.cs
@@ -1,8 +1,37 @@
+using Brainstorm.Data.Sessions;
+using Brainstorm.Web.Factories;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Brainstorm.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Logging.AddSimpleConsole(options =>
+{
+    options.IncludeScopes = true;
+    options.SingleLine = false;
+    options.TimestampFormat = "[hh:mm:ss.fff] ";
+    options.ColorBehavior = LoggerColorBehavior.Default;
+});
+
+builder.Services.AddDbContext<BrainstormDbContext>(o =>
+{
+    o.UseSqlite("Data Source=brainstorm.db;Cache=Shared;Mode=ReadWriteCreate");
+});
+
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(o =>
+    {
+        o.LoginPath = "/Auth/Login";
+        o.AccessDeniedPath = "/Auth/AccessDenied";
+    });
 
 var app = builder.Build();
 
@@ -16,15 +45,44 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+
+
+app.Map("home/canvas/{sessionId}/ws", async (HttpContext context, string sessionId) =>
+{
+    if (!SessionFactory.SessionExists(sessionId).Result)
+    {
+        context.Response.StatusCode = 404;
+        return;
+    }
+    
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        Console.WriteLine($"WebSocket request on session: {sessionId}");
+        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        var handler = WebSocketHandlerFactory.CreateOrGet(sessionId);
+        await handler.Handle(context, webSocket);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+    }
+});
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Auth}/{action=Login}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BrainstormDbContext>();
+    db.Database.EnsureCreated();
+    db.Database.ExecuteSqlRaw("PRAGMA journal_mode=WAL;");
+}
 
 app.Run();
+
 
 public partial class Program {}
